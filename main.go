fc0c3614d7ec1e9425af50fc0dbc54b01425652b
Aman Jain <aman.jain@cross-border.jp>
2024-02-26 15:12:54 +0530

    Dummy GRPC

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	helloworldpb "github.com/amanjain-cb/grpc-server/gen/go/proto/helloworld"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	helloworldpb.UnimplementedGreeterServer
+	helloworldpb.UnimplementedGreeter2Server
+}
+
+func (s *server) SayHello(ctx context.Context, in *helloworldpb.HelloRequest) (*helloworldpb.HelloReply, error) {
+	return &helloworldpb.HelloReply{Message: in.Name + " world"}, nil
+}
+
+func (s *server) SayHello2(ctx context.Context, in *helloworldpb.HelloRequest2) (*helloworldpb.HelloReply2, error) {
+	return &helloworldpb.HelloReply2{Message: "GET HELLO WORLD"}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalln("Failed to listen:", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	helloworldpb.RegisterGreeterServer(grpcServer, &server{})
+	helloworldpb.RegisterGreeter2Server(grpcServer, &server{})
+
+	log.Println("Serving gRPC on 0.0.0.0:9000")
+	err = grpcServer.Serve(lis)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
