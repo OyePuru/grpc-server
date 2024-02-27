94a09a8e7bbd712e6ad5eff6eb12797ea16efb8f
Aman Jain <aman.jain@cross-border.jp>
2024-02-27 13:04:02 +0530

    update grpc server

--- go.mod
+++ go.mod
@@ -3,12 +3,11 @@ module github.com/amanjain-cb/grpc-server
 go 1.22.0
 
 require (
+	github.com/OyePuru/grpc-proto v0.0.0-20240227070444-9812b4182514
 	google.golang.org/grpc v1.62.0
-	google.golang.org/protobuf v1.32.0
 )
 
 require (
-	github.com/OyePuru/grpc-proto v0.0.0-20240226151816-41ad8e8a84c5 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.19.1 // indirect
 	golang.org/x/net v0.20.0 // indirect
@@ -16,4 +15,5 @@ require (
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20240125205218-1f4bbc51befe // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240125205218-1f4bbc51befe // indirect
+	google.golang.org/protobuf v1.32.0 // indirect
 )
--- main.go
+++ main.go
@@ -5,36 +5,48 @@ import (
 	"log"
 	"net"
 
-	helloworldpb "github.com/OyePuru/grpc-proto/gen/go/client/proto/helloworld"
+	// Importing the auto-generated gRPC client package from grpcproto
+	grpcprotopb "github.com/OyePuru/grpc-proto/gen/go/client/proto/grpcproto"
 	"google.golang.org/grpc"
 )
 
 type server struct {
-	helloworldpb.UnimplementedGreeterServer
-	helloworldpb.UnimplementedGreeter2Server
+	// Embedding the generated interface services
+	grpcprotopb.UnimplementedGreeterServer
+	grpcprotopb.UnimplementedGreeter2Server
 }
 
-func (s *server) SayHello(ctx context.Context, in *helloworldpb.HelloRequest) (*helloworldpb.HelloReply, error) {
-	return &helloworldpb.HelloReply{Message: in.Name + " world"}, nil
+// Implementation of the SayHello method for the Greeter service
+func (s *server) SayHello(ctx context.Context, in *grpcprotopb.HelloRequest) (*grpcprotopb.HelloReply, error) {
+	// Concatenate the received name with " world" and return
+	return &grpcprotopb.HelloReply{Message: in.Name + " world"}, nil
 }
 
-func (s *server) SayHello2(ctx context.Context, in *helloworldpb.HelloRequest2) (*helloworldpb.HelloReply2, error) {
-	return &helloworldpb.HelloReply2{Message: "GET HELLO WORLD"}, nil
+// Implementation of the SayHello2 method for the Greeter2 service
+func (s *server) SayHello2(ctx context.Context, in *grpcprotopb.HelloRequest2) (*grpcprotopb.HelloReply2, error) {
+	// Return a predefined message
+	return &grpcprotopb.HelloReply2{Message: "GET HELLO WORLD"}, nil
+}
+
+// Register the Greeter service with the server
+func RegisterGrpcServicesWithServer(grpcServer grpc.ServiceRegistrar) {
+	grpcprotopb.RegisterGreeterServer(grpcServer, &server{})
+	grpcprotopb.RegisterGreeter2Server(grpcServer, &server{})
 }
 
 func main() {
+	// Create a listener on TCP port 9000
 	lis, err := net.Listen("tcp", ":9000")
 	if err != nil {
 		log.Fatalln("Failed to listen:", err)
 	}
 
+	// Create a new gRPC server
 	grpcServer := grpc.NewServer()
-	helloworldpb.RegisterGreeterServer(grpcServer, &server{})
-	helloworldpb.RegisterGreeter2Server(grpcServer, &server{})
+	RegisterGrpcServicesWithServer(grpcServer)
 
 	log.Println("Serving gRPC on 0.0.0.0:9000")
-	err = grpcServer.Serve(lis)
-	if err != nil {
+	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatal(err)
 	}
 }
